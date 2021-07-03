380120c040a9d93a367c150588bc12467d50cc2b
Daniel Rodrigues <munizbarreto5@gmail.com>
2021-07-03 20:15:34 -0300

    refactor: adjustment middlewares

--- src/infra/config/routes-config.ts
+++ src/infra/config/routes-config.ts
@@ -4,6 +4,6 @@ import fg from 'fast-glob'
 export default (app: Express): void => {
   const router = Router()
   app.use('/v1', router)
-  fg.sync('**/src/infra/routes/**/**routes.ts')
+  fg.sync('**/src/infra/routes/**routes.ts')
     .map(async file => (await import(`../../../${file}`)).default(router))
 }
--- tests/infra/routes/routes-patient.test.ts
+++ tests/infra/routes/routes-patient.test.ts
@@ -2,8 +2,8 @@ import app from '../../../src/infra/config/app'
 
 import request from 'supertest'
 
-describe('Content Type Middleware', () => {
-  test('Should return default content type as json', async () => {
+describe('Routes', () => {
+  test('should return response into create', async () => {
     await request(app)
       .get('/v1/patient/create')
       .expect({ ok: 'ok' })
