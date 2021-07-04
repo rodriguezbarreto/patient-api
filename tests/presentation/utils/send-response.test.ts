bae93c190b21efb7b336329db80a88f7035cdb4d
Daniel Rodrigues <munizbarreto5@gmail.com>
2021-07-04 15:56:36 -0300

    test: add test messagem custom on sendResponse

--- tests/presentation/utils/send-response.test.ts
+++ tests/presentation/utils/send-response.test.ts
@@ -2,9 +2,15 @@ import { sendResponse } from '../../../src/presentation/utils-presentation'
 import { dataMock, successSendResponseMock } from './mocks/send-response-mock'
 
 describe('Send Response Utils', () => {
-  test('Should return error message: "Internal Server Error"', () => {
+  test('should return default error message', () => {
     const result = sendResponse(500)
     expect(result.error).toEqual('Internal Server Error')
+  })
+
+  test('should return custom error message', () => {
+    const dataErrorMessageMock = 'Promisse Reject'
+    const result = sendResponse(500, dataErrorMessageMock)
+    expect(result.error).toEqual(dataErrorMessageMock)
   })
 
   test('Should returt status 200 and data', () => {
