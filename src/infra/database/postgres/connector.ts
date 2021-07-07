68e74c8986d708a789f7a5ecb25f7875f62ae90a
Daniel Rodrigues <munizbarreto5@gmail.com>
2021-07-07 15:42:04 -0300

    feat: add config typeorm

--- src/infra/database/postgres/connector.ts
+++ src/infra/database/postgres/connector.ts
@@ -1,8 +1,20 @@
 import { createConnection } from 'typeorm'
+import { PatientModel } from './models/patient-model'
 
 export const connectionPostgres = async (): Promise<void> => {
-  const connection = await createConnection()
-  console.log(`--> Connection with database: ON (database: ${connection.options.database})`)
-
-  process.on('SIGINT', async () => await connection.close().then(() => console.log('--> Connection with database: OFF')))
+  try {
+    const connection = await createConnection({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'pebmedapi',
+      entities: [PatientModel]
+    })
+    console.log(`--> Connection with database: ON (database: ${connection.options.database})`)
+    process.on('SIGINT', async () => await connection.close().then(() => console.log('--> Connection with database: OFF')))
+  } catch (err) {
+    console.log(err)
+  }
 }
--- src/infra/database/postgres/models/patient-model.ts
+++ src/infra/database/postgres/models/patient-model.ts
@@ -1,16 +1,8 @@
 import { Patient } from '../../../../domain/entities/patient'
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 
-@Entity('patient')
+@Entity('patients')
 export class PatientModel implements Patient {
-  constructor (patient: Patient) {
-    this.name = patient.name
-    this.birthDate = patient.birthDate
-    this.phone = patient.phone
-    this.height = patient.height
-    this.weight = patient.weight
-  }
-
   @PrimaryGeneratedColumn('uuid')
   id: string
 
