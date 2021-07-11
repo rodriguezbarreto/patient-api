1968abfefbaacaa4a3a475b258972c528f44d1fd
Daniel Barreto <daniel.barreto@olist.com>
2021-07-11 10:25:23 -0300

    test: add unit test of list patient service

---
+++ tests/data/list-patients-service.test.ts
@@ -0,0 +1,59 @@
+import { ListPatientsRepository, ListPatientsService } from '../../src/data'
+import { Patient } from '../../src/domain'
+import { PatientModel } from '../../src/infra/database'
+
+const fakePatientsList: PatientModel[] = [
+  {
+    id: 'testDeId',
+    name: 'Daniel',
+    birthDate: '28/02/1988',
+    phone: '48996366726',
+    height: 180,
+    weight: 98.6
+  },
+  {
+    id: 'testDeId2',
+    name: 'Fabio',
+    birthDate: '28/02/1988',
+    phone: '48996366745',
+    height: 180,
+    weight: 98.6
+  }
+]
+
+interface SutTypes {
+  sut: ListPatientsService
+  fakeListPatientsRespository: ListPatientsRepository
+}
+
+const mockListPatientsRepository = (): ListPatientsRepository => {
+  class FakeListPatientsRespository implements ListPatientsRepository {
+    async listPatients (): Promise<Patient[]> {
+      return await new Promise(resolve => resolve(fakePatientsList))
+    }
+  }
+
+  return new FakeListPatientsRespository()
+}
+
+const mockSut = (): SutTypes => {
+  const fakeListPatientsRespository = mockListPatientsRepository()
+  const sut = new ListPatientsService(fakeListPatientsRespository)
+  return { sut, fakeListPatientsRespository }
+}
+
+describe('Create Patient Service', () => {
+  test('should call ListPatientsRespository correctly', () => {
+    const { sut, fakeListPatientsRespository } = mockSut()
+    const repositorySpy = jest.spyOn(fakeListPatientsRespository, 'listPatients')
+    sut.list()
+    expect(repositorySpy).toHaveBeenCalled()
+  })
+
+  test('throw an exception if ListPatientsRespository throws', async () => {
+    const { sut, fakeListPatientsRespository } = mockSut()
+    jest.spyOn(fakeListPatientsRespository, 'listPatients').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.list()
+    expect(promise).rejects.toThrow()
+  })
+})
